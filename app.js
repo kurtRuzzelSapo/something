6b688adec4c6ae7bdfe7caecdd18cead3f261be7
kurtRuzzelSapo <wkurtruzzelsapo@gmail.com>
2024-02-11 20:36:20 +0800

    Added comments to the app js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let btnYes = document.querySelector('.yes');
 let btnNo = document.querySelector('.no');
 let picture = document.querySelector('.picture');
 let msg = document.querySelector('.message');
+
+// When click the btnYes and btnNo button will remove and the picture and text will change
 btnYes.addEventListener('click', function () {
 	btnYes.classList.add('hide');
 	btnNo.classList.add('hide');
@@ -11,7 +13,8 @@ btnYes.addEventListener('click', function () {
 });
 
 let width = 100;
-let height = 40;
+let height = 100;
+//When click the btnYES will increase the width and height each click and the picture will change
 btnNo.addEventListener('click', function () {
 	btnYes.style.width = `${width}px`;
 	btnYes.style.height = `${height}px`;
